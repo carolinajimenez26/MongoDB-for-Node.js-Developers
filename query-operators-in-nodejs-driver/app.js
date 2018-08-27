260516c029fe922509471131d5eab98b130bbac8
Carolina <carolina.jimenez.g@gmail.com>
2018-08-26 21:08:39 -0500

    Implements function arrows

--- query-operators-in-nodejs-driver/app.js
+++ query-operators-in-nodejs-driver/app.js
@@ -1,77 +1,75 @@
-var MongoClient = require('mongodb').MongoClient,
-    commandLineArgs = require('command-line-args'), 
-    assert = require('assert');
+const MongoClient = require('mongodb').MongoClient,
+      commandLineArgs = require('command-line-args'),
+      assert = require('assert');
 
 
-var options = commandLineOptions();
+const options = commandLineOptions();
 
-MongoClient.connect('mongodb://localhost:27017/crunchbase', function(err, db) {
+MongoClient.connect('mongodb://localhost:27017/crunchbase', (err, db) => {
 
-    assert.equal(err, null);
-    console.log("Successfully connected to MongoDB.");
-    
-    var query = queryDocument(options);
-    var projection = {"_id": 1, "name": 1, "founded_year": 1,
-                      "number_of_employees": 1, "crunchbase_url": 1};
+  assert.equal(err, null);
+  console.log("Successfully connected to MongoDB.");
 
-    var cursor = db.collection('companies').find(query, projection);
-    var numMatches = 0;
+  let query = queryDocument(options);
+  let projection = {"_id": 1, "name": 1, "founded_year": 1,
+                    "number_of_employees": 1, "crunchbase_url": 1};
 
-    cursor.forEach(
-        function(doc) {
-            numMatches = numMatches + 1;
-            console.log( doc );
-        },
-        function(err) {
-            assert.equal(err, null);
-            console.log("Our query was:" + JSON.stringify(query));
-            console.log("Matching documents: " + numMatches);
-            return db.close();
-        }
-    );
+  let cursor = db.collection('companies').find(query, projection);
+  let numMatches = 0;
+
+  cursor.forEach(
+    (doc) => {
+      numMatches = numMatches + 1;
+      console.log( doc );
+    },
+    (err) => {
+      assert.equal(err, null);
+      console.log("Our query was:" + JSON.stringify(query));
+      console.log("Matching documents: " + numMatches);
+      return db.close();
+    }
+  );
 
 });
 
 
 function queryDocument(options) {
 
-    console.log(options);
-    
-    var query = {
-        "founded_year": {
-            "$gte": options.firstYear,
-            "$lte": options.lastYear
-        }
-    };
+  console.log(options);
 
-    if ("employees" in options) {
-        query.number_of_employees = { "$gte": options.employees };
+  let query = {
+    "founded_year": {
+      "$gte": options.firstYear,
+      "$lte": options.lastYear
     }
-        
-    return query;
-    
+  };
+
+  if ("employees" in options) {
+    query.number_of_employees = { "$gte": options.employees };
+  }
+
+  return query;
+
 }
 
 
 function commandLineOptions() {
 
-    var cli = commandLineArgs([
-        { name: "firstYear", alias: "f", type: Number },
-        { name: "lastYear", alias: "l", type: Number },
-        { name: "employees", alias: "e", type: Number }
-    ]);
-    
-    var options = cli.parse()
-    if ( !(("firstYear" in options) && ("lastYear" in options))) {
-        console.log(cli.getUsage({
-            title: "Usage",
-            description: "The first two options below are required. The rest are optional."
-        }));
-        process.exit();
-    }
+  let cli = commandLineArgs([
+      { name: "firstYear", alias: "f", type: Number },
+      { name: "lastYear", alias: "l", type: Number },
+      { name: "employees", alias: "e", type: Number }
+  ]);
 
-    return options;
-    
+  let options = cli.parse()
+  if ( !(("firstYear" in options) && ("lastYear" in options))) {
+    console.log(cli.getUsage({
+      title: "Usage",
+      description: "The first two options below are required. The rest are optional."
+    }));
+    process.exit();
+  }
+
+  return options;
+
 }
-
-
